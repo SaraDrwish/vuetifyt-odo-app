da6c58c0af79f0b19b5fec6a251642a44c0a7d6c
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-20 22:43:27 +0300

    3

--- src/App.vue
+++ src/App.vue
@@ -10,35 +10,35 @@
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title>
-            <span>title</span>
+            <h3>Vuetify Todo</h3>
           </v-list-item-title>
           <v-list-item-subtitle>
-            <span>subtitle</span>
+            <span>best todo ever !</span>
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
+
       <v-list density="compact" nav>
-        <v-list-item>
-          <v-btn text>
-            <v-icon>mdi-account</v-icon>
-          </v-btn>
-          <span>shared </span>
+        <!-- ///// -->
+        <!-- <v-list-item link>
+          <v-icon>mdi-view-dashboard</v-icon>
+          <span>Todo </span>
         </v-list-item>
 
-        <v-list-item>
+        <v-list-item link>
           <v-btn text>
             <v-icon>mdi-account-multiple</v-icon>
           </v-btn>
-          <span>shared with me </span>
-        </v-list-item>
-
-        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred">
-          <v-btn text>
-            <v-icon>mdi-folder</v-icon>
-          </v-btn>
-          <span>My Files</span>
+          <span>About</span>
+        </v-list-item> -->
+        <!-- ///////// -->
+        <v-list-item v-for="it in items" :key="it" link>
+          <v-list-item-icon>
+            <v-icon> {{ it.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>{{ it.title }}</v-list-item-content>
         </v-list-item>
       </v-list>
       <!-- <span>sdfdsfsd</span> -->
@@ -52,7 +52,7 @@
 
     <v-main>
       <!--  -->
-      <span class="pa-8 red yellow--text"> helooo </span>
+      <span class="pa-8 red yellow--text"> Helooo </span>
     </v-main>
   </v-app>
 </template>
@@ -62,6 +62,11 @@ export default {
   data() {
     return {
       drawer: null,
+      items: [
+        { id: 1, title: "Dashboard", icon: "mdi-view-dashboard" },
+        { id: 2, title: "About", icon: "mdi-account" },
+        { id: 3, title: "Star", icon: "mdi-star" },
+      ],
     };
   },
 };
